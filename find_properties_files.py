ad85e1734a4704d23ba5dca4638338a11c7fcca4
Andreas Kring <kring@openforce.dk>
2016-04-14 19:45:58 +0200

    Added code to generate updated danish dictionary

--- find_properties_files.py
+++ find_properties_files.py
@@ -1,14 +1,17 @@
 from os import walk
 from sys import argv
 from os.path import join, splitext, relpath
+from pickle import dump
 
 EXTENSION = '.properties'
 LANG_EXT = 'fr'
 NOT_BLANK = 'not blank'
 MAX_VALUE_LENGTH = 100
 
-alfresco_source = '/home/andreas/alfresco-source/share/share/src/main/resources'
-translations_source = '/home/andreas/alfresco-danish/share/src/main/amp/config'
+# alfresco_source = '/home/andreas/alfresco-source/share/share/src/main/resources'
+alfresco_source = '/mnt/sdb1/magenta/alfresco-source/share/share/src/main/resources'
+# translations_source = '/home/andreas/magenta/alfresco-danish/share/src/main/amp/config'
+translations_source = '/mnt/sdb1/magenta/alfresco-danish/share/src/main/amp/config'
 
 def not_blank(line):
     if line == '':
@@ -77,64 +80,56 @@ def unique_properties(tuples):
                 print t[0],'\t', d[t[0]], '\t',t[1]
     return d
 
-def generate_new_danish_dictionary()
+### Generate CSV file ###
+def generate_csv(sorted_filenames):
+    lines = []
+    for tup in sorted_filenames:
+        if tup[5] in danish_dictionary:
+            pass
+            lines.append(tup[0] + '; '+ tup[1][:MAX_VALUE_LENGTH] + '; ' + danish_dictionary[tup[5]][:MAX_VALUE_LENGTH] + '; ' + tup[5] + '\n')
+        else:
+            lines.append(tup[0] + '; '+ tup[1][:MAX_VALUE_LENGTH] + '; ; ' + tup[5] + '\n')
 
-"""
-### Generate new danish dict
-loop over uid
-  if uid not in danish_dict
-    if not value for uid is empty in english_dict
-      if value not empty in csv_dict
-        print englist value
-        print danish value
-        ask: update danish value?
-        if ok
-          update danish_dict
-        else
-          ask for replacement
-          if not stop
-            update danish_dict
-          else
-            add uid to list
-        write new danish_dict to file
-        write uid list of mistakes 
-"""
+    with open('properties.csv', 'w') as f:
+        f.writelines(lines)
+
+def generate_new_danish_dictionary(english_dict, danish_dict, csv_dict):
+    danish_dict_additions = {}
+    translation_mistakes_uid = []
+    for uid in english_dict.keys():
+        if uid not in danish_dict.keys() and english_dict[uid] != '' and csv_dict[uid] != '':
+            print 50*'-'
+            print uid
+            print english_dict[uid]
+            print csv_dict[uid]
+            q = raw_input('Use danish value above (Y/n)? ')
+            if q in ('Y', 'y', ''):
+                danish_dict_additions[uid] = csv_dict[uid]
+            elif q == 'n':
+                translation_mistakes_uid.append(uid)
+                replacement = raw_input('Enter alternative danish translation: ')
+                if replacement != 'stop':
+                    danish_dict_additions[uid] = replacement
+                else:
+                    return danish_dict_additions, translation_mistakes_uid
+            else:
+                return danish_dict_additions, translation_mistakes_uid
 
 filenames = find_files(alfresco_source, LANG_EXT)
 filenames_da = find_files(translations_source, 'da')
 
-english_dictionary = dict((x[5], x[1]) for x in filenames)
-danish_dictionary = dict((x[5], x[1]) for x in filenames_da)
+e = english_dictionary = dict((x[5], x[1]) for x in filenames)
+d = danish_dictionary = dict((x[5], x[1]) for x in filenames_da)
 
 csv = read_properties_from_csv('properties_corrected.csv')
-csv_dictionary = dict((x[3], x[2]) for x in csv)
+c = csv_dictionary = dict((x[3], x[2]) for x in csv)
 
 sorted_filenames = sorted(filenames, key = lambda filename: filename[0])
+generate_csv(sorted_filenames)
 
-### Generate CSV file ###
-lines = []
-for tup in sorted_filenames:
-    if tup[5] in danish_dictionary:
-        pass
-        lines.append(tup[0] + '; '+ tup[1][:MAX_VALUE_LENGTH] + '; ' + danish_dictionary[tup[5]][:MAX_VALUE_LENGTH] + '; ' + tup[5] + '\n')
-    else:
-        lines.append(tup[0] + '; '+ tup[1][:MAX_VALUE_LENGTH] + '; ; ' + tup[5] + '\n')
-
-with open('properties.csv', 'w') as f:
-    f.writelines(lines)
-
-### Find the maximum length of the values
-# values_lengths = [len(x[1]) for x in sorted_filenames]
-# print max(values_lengths) 
+da_dict_add, mistakes = generate_new_danish_dictionary(english_dictionary, danish_dictionary, csv_dictionary)
+with open('xxxda_dict_add.dat', 'w') as f:
+    dump((da_dict_add, mistakes), f)
 
 
 
-"""
-for tup in csv:
-    uid = tup[-1]
-    try:
-        if tup[1] != english_dictionary[uid]:
-            print uid
-    except:
-        print tup
-"""
