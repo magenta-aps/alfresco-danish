3edbcebac95e2541872f257cdb3bfcb98f16ae2f
Andreas Kring <andreas@magenta.dk>
2016-04-19 16:00:13 +0200

    Work on making the script more user friendly

--- find_properties_files.py
+++ find_properties_files.py
@@ -1,30 +1,20 @@
 # -*- coding: utf-8 -*-
 
+##################################################################################################
+# Usage syntax:
+# $ python find_properties_files.py -g /path/to/alfresco-source/share/src/main/resources /path/to/alfresco-danish/share/src/main/amp/config
+##################################################################################################
+
 from os import walk, makedirs
-from os.path import join, splitext, relpath, isfile, dirname, exists
+from os.path import join, splitext, relpath, isfile, dirname, exists, isdir
 from pickle import dump, load
 from sys import argv
 
+CONFIG_FILE = 'settings.conf'
 
-EXTENSION = '.properties'
-LANG_EXT = 'fr'
 NOT_BLANK = 'not blank'
-MAX_VALUE_LENGTH = 200
-CSV_SEPARATOR = ";"
 
-# alfresco_source = '/home/andreas/alfresco-source/share/share/src/main/resources'
-# alfresco_source = 'home/andreas/alfresco-5.1.e/tomcat/webapps/share/WEB-INF/classes'
-# translations_source = '/home/andreas/alfresco-danish/share/src/main/amp/config'
-
-### Aikau ###
-alfresco_source = '/home/andreas/alfresco-source/Aikau/aikau/src/main/resources'
-translations_source = '/home/andreas/alfresco-danish/share/src/main/amp/web/js/aikau/LATEST'
-
-
-# alfresco_source = '/mnt/sdb1/magenta/alfresco-source/share/share/src/main/resources'
-# translations_source = '/mnt/sdb1/magenta/alfresco-danish/share/src/main/amp/config'
-
-def not_blank(line):
+def __not_blank(line):
     if line == '':
         line = NOT_BLANK
     return line
@@ -48,7 +38,7 @@ def get_properties(properties_file):
                 properties[key] = value
             except:
                 print properties_file
-        previous_line = not_blank(line)
+        previous_line = __not_blank(line)
     return properties
 
 def find_files(path, lang_ext):
@@ -80,16 +70,6 @@ def read_properties_from_csv(filename, csv_sep):
     content = [line.split(csv_sep)[:4] for line in lines]
     content = [tuple([y.strip() for y in x]) for x in content]
     return content
-
-def unique_properties(tuples):
-    d = {}
-    for t in tuples:
-        if not t[0] in d:
-            d[t[0]] = [t[1]]
-        else:
-            if not d[t[0]] == t[1]:
-                print t[0],'\t', d[t[0]], '\t',t[1]
-    return d
 
 ### Generate CSV file ###
 def generate_csv(sorted_filenames, separator = CSV_SEPARATOR):
@@ -136,17 +116,49 @@ def dump_dict(filename, dictionary, mistakes):
     with open(filename, 'w') as f:
         dump((dictionary, mistakes), f)
 
-filenames = find_files(alfresco_source, LANG_EXT)
-filenames_da = find_files(translations_source, 'da')
+def update_properties_files_with_new_danish_values(danish_dict_additions, path_prefix):
+    for uid in danish_dict_additions.keys():
+        path, label = uid.split('}')
+        path = path[1:]
+        full_path = join(path_prefix, path)
+        full_path = full_path.replace(EXTENSION, '_da' + EXTENSION)
+        # print full_path
+        
+        print uid
+        if isfile(full_path):
+            print 'File already exists'
+            with open(full_path, 'a') as f:
+                f.write('\n' + label + '=' + danish_dict_additions[uid] + '\n')
+        else:
+            print 'Creating new file'
+            if not exists(dirname(full_path)):
+                makedirs(dirname(full_path))
+            with open(full_path, 'w') as f:
+                f.write(label + '=' + danish_dict_additions[uid] + '\n')
+            # raw_input('tryk')
 
-e = english_dictionary = dict((x[5], x[1]) for x in filenames)
-d = danish_dictionary = dict((x[5], x[1]) for x in filenames_da)
+
+
+if '-g' in argv and len(argv) == 4 and isdir(argv[-2]) and isdir(argv[-1]):
+    # Generate spreadsheet to compare and fill in missing translations (output to properties.cvs)
+    filenames = find_files(alfresco_source, LANG_EXT)
+    filenames_da = find_files(translations_source, 'da')
+    english_dictionary = dict((x[5], x[1]) for x in filenames)
+    danish_dictionary = dict((x[5], x[1]) for x in filenames_da)
+    sorted_filenames = sorted(filenames, key = lambda filename: filename[0])
+    generate_csv(sorted_filenames)
+elif '-n' in argv and len(argv) == 4 and isfile(argv[-2]) and isfile(argv[-1]):
+    pass
+    # 
+    # da_dict_add, mistakes = generate_new_danish_dictionary(english_dictionary, danish_dictionary, csv_dictionary, da_dict_add, mistakes)
+else:
+    print 'Wrong syntax'
+    exit()
+
 
 # csv = read_properties_from_csv('properties_corrected2.csv', CSV_SEPARATOR)
 # c = csv_dictionary = dict((x[3], x[2]) for x in csv)
 
-sorted_filenames = sorted(filenames, key = lambda filename: filename[0])
-generate_csv(sorted_filenames)
 
 # da_dict_add, mistakes = load_dict('da_dict_add_final.dat')
 # da_dict_add, mistakes = generate_new_danish_dictionary(english_dictionary, danish_dictionary, csv_dictionary, da_dict_add, mistakes)
@@ -192,26 +204,6 @@ generate_csv(sorted_filenames)
 #     da_dict_add[key] = da_dict_add[key].replace('Å', '\\u00C5')
 
 
-def update_properties_files_with_new_danish_values(danish_dict_additions, path_prefix):
-    for uid in danish_dict_additions.keys():
-        path, label = uid.split('}')
-        path = path[1:]
-        full_path = join(path_prefix, path)
-        full_path = full_path.replace(EXTENSION, '_da' + EXTENSION)
-        # print full_path
-        
-        print uid
-        if isfile(full_path):
-            print 'File already exists'
-            with open(full_path, 'a') as f:
-                f.write('\n' + label + '=' + danish_dict_additions[uid] + '\n')
-        else:
-            print 'Creating new file'
-            if not exists(dirname(full_path)):
-                makedirs(dirname(full_path))
-            with open(full_path, 'w') as f:
-                f.write(label + '=' + danish_dict_additions[uid] + '\n')
-            # raw_input('tryk')
 
 # update_properties_files_with_new_danish_values(da_dict_add, translations_source)
 
