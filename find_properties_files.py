992848579f5cef280d777b18a0f3ecda718e4d27
AlexanderMomtaz <alexander@magenta.dk>
2016-08-19 16:23:20 +0200

    Updated the script to be more flexible

--- find_properties_files.py
+++ find_properties_files.py
@@ -1,28 +1,35 @@
 # -*- coding: utf-8 -*-
 
-from os import walk, makedirs
+from os import walk, makedirs, path, getcwd
 from os.path import join, splitext, relpath, isfile, dirname, exists
 from pickle import dump, load
-from sys import argv
-
+import argparse
 
 EXTENSION = '.properties'
 LANG_EXT = 'fr'
 NOT_BLANK = 'not blank'
 MAX_VALUE_LENGTH = 200
-CSV_SEPARATOR = ";"
+CSV_SEPARATOR = "~"
 
-# alfresco_source = '/home/andreas/alfresco-source/share/share/src/main/resources'
-# alfresco_source = 'home/andreas/alfresco-5.1.e/tomcat/webapps/share/WEB-INF/classes'
-# translations_source = '/home/andreas/alfresco-danish/share/src/main/amp/config'
+# Get arguments for paths
+parser = argparse.ArgumentParser(description='This is a script by Magenta')
+parser.add_argument('-s','--share', help='Share source path',required=True)
+parser.add_argument('-a','--aikau', help='Aikau source path',required=True)
+parser.add_argument('-t', '--translate', action='store_true')
+parser.add_argument('-i', '--inject', action='store_true')
+parser.add_argument('-e', '--extract', action='store_true')
+args = parser.parse_args()
 
-### Aikau ###
-alfresco_source = '/home/andreas/alfresco-source/Aikau/aikau/src/main/resources'
-translations_source = '/home/andreas/alfresco-danish/share/src/main/amp/web/js/aikau/LATEST'
 
+# Find path for script
+rootDir = getcwd()
+fileDir = path.dirname(path.abspath(__file__))
 
-# alfresco_source = '/mnt/sdb1/magenta/alfresco-source/share/share/src/main/resources'
-# translations_source = '/mnt/sdb1/magenta/alfresco-danish/share/src/main/amp/config'
+# Set paths for sources
+share_source = path.join(rootDir, args.share + 'share/src/main/resources')
+aikau_source = path.join(rootDir, args.aikau + 'aikau/src/main/resources')
+share_translations = path.join(fileDir, 'share/src/main/amp/config')
+aikau_translations = path.join(fileDir, 'share/src/main/amp/web/js/aikau/LATEST')
 
 def not_blank(line):
     if line == '':
@@ -74,7 +81,7 @@ def find_files(path, lang_ext):
 
 
 def read_properties_from_csv(filename, csv_sep):
-    with open(filename, 'r') as f:
+    with open(fileDir + '/' + filename, 'r') as f:
         lines = f.readlines()
     # content = tuple([x.strip() for x in [line.split(';')[:4] for line in lines]])
     content = [line.split(csv_sep)[:4] for line in lines]
@@ -92,126 +99,87 @@ def unique_properties(tuples):
     return d
 
 ### Generate CSV file ###
-def generate_csv(sorted_filenames, separator = CSV_SEPARATOR):
-    separator += ' '
+def generate_csv(prefix, sorted_filenames, da_dictionary, separator = CSV_SEPARATOR):
     lines = []
     for tup in sorted_filenames:
-        if tup[5] in danish_dictionary:
-            pass
-            lines.append(tup[0] + separator+ tup[1][:MAX_VALUE_LENGTH] + separator + danish_dictionary[tup[5]][:MAX_VALUE_LENGTH] + separator + tup[5] + '\n')
-        else:
-            lines.append(tup[0] + separator+ tup[1][:MAX_VALUE_LENGTH] + separator + tup[5] + '\n')
+        danish_str = ''
+        if tup[5] in da_dictionary:
+            danish_str = da_dictionary[tup[5]][:MAX_VALUE_LENGTH]
+        lines.append(tup[0] + separator+ tup[1][:MAX_VALUE_LENGTH] + separator + danish_str + separator + tup[5] + '\n')
 
-    with open('properties.csv', 'w') as f:
+    with open(fileDir + '/' + prefix + '_properties.csv', 'w') as f:
         f.writelines(lines)
 
-def generate_new_danish_dictionary(english_dict, danish_dict, csv_dict, danish_dict_additions = {}, translation_mistakes_uid = []):
-    print len(english_dict.keys())
-    print len(danish_dict.keys())
-    print len(danish_dict_additions.keys())
+def generate_new_danish_dictionary(en_dict, da_dict, csv_dict, da_dict_add = {}, translation_mistakes_uid = []):
+    print len(en_dict.keys())
+    print len(da_dict.keys())
+    print len(da_dict_add.keys())
     print len(translation_mistakes_uid)
-    for uid in english_dict.keys():
+    for uid in en_dict.keys():
         print uid
-        if uid not in danish_dict.keys() and uid not in danish_dict_additions.keys() and english_dict[uid] != '' and csv_dict[uid] != '':
+        if uid not in da_dict.keys() and uid not in da_dict_add.keys() and en_dict[uid] != '' and csv_dict[uid] != '':
             print 50*'-'
             print uid
-            print english_dict[uid]
+            print en_dict[uid]
             print csv_dict[uid]
             q = raw_input('Use danish value above (Y/n)? ')
             if q in ('Y', 'y', ''):
-                danish_dict_additions[uid] = csv_dict[uid]
+                da_dict_add[uid] = csv_dict[uid]
             elif q == 'n':
                 translation_mistakes_uid.append(uid)
                 replacement = raw_input('Enter alternative danish translation: ')
-                danish_dict_additions[uid] = replacement
+                da_dict_add[uid] = replacement
             else:
-                return danish_dict_additions, translation_mistakes_uid
-    return danish_dict_additions, translation_mistakes_uid
+                return da_dict_add, translation_mistakes_uid
+    return da_dict_add, translation_mistakes_uid
 
-def load_dict(filename):
-    with open(filename, 'r') as f:
-        return load(f)
-
-def dump_dict(filename, dictionary, mistakes):
-    with open(filename, 'w') as f:
-        dump((dictionary, mistakes), f)
-
-filenames = find_files(alfresco_source, LANG_EXT)
-filenames_da = find_files(translations_source, 'da')
-
-e = english_dictionary = dict((x[5], x[1]) for x in filenames)
-d = danish_dictionary = dict((x[5], x[1]) for x in filenames_da)
-
-# csv = read_properties_from_csv('properties_corrected2.csv', CSV_SEPARATOR)
-# c = csv_dictionary = dict((x[3], x[2]) for x in csv)
-
-sorted_filenames = sorted(filenames, key = lambda filename: filename[0])
-generate_csv(sorted_filenames)
-
-# da_dict_add, mistakes = load_dict('da_dict_add_final.dat')
-# da_dict_add, mistakes = generate_new_danish_dictionary(english_dictionary, danish_dictionary, csv_dictionary, da_dict_add, mistakes)
-# 
-# dump_dict('da_dict_add.dat', da_dict_add, mistakes)
-
-
-
-# lines = []
-# for tup in sorted_filenames:
-#     if tup[5] in da_dict_add.keys() and da_dict_add[tup[5]] == 'TODO':
-#         lines.append(tup[0] + CSV_SEPARATOR + tup[1][:MAX_VALUE_LENGTH] + CSV_SEPARATOR + ' ' + CSV_SEPARATOR + tup[5] + '\n')
-# 
-# with open('properties.csv', 'w') as f:
-#     f.writelines(lines)
-
-### Add corrections 2 ###
-
-# for tup in csv:
-#     uid = tup[-1]
-#     print 50*'-'
-#     print uid
-#     print english_dictionary[uid]
-#     print tup[2]
-#     q = raw_input('Use danish value above (Y/n)? ')
-#     if q in ('Y', 'y', ''):
-#         da_dict_add[uid] = tup[2]
-#     elif q == 'n':
-#         replacement = raw_input('Enter alternative danish translation: ')
-#         da_dict_add[uid] = replacement
-#     else:
-#         raise
-# 
-# dump_dict('da_dict_add_final.dat', da_dict_add, [])
-
-
-# for key in da_dict_add.keys():
-#     da_dict_add[key] = da_dict_add[key].replace('æ', '\\u00E6')
-#     da_dict_add[key] = da_dict_add[key].replace('ø', '\\u00F8')
-#     da_dict_add[key] = da_dict_add[key].replace('å', '\\u00E5')
-#     da_dict_add[key] = da_dict_add[key].replace('Æ', '\\u00C6')
-#     da_dict_add[key] = da_dict_add[key].replace('Ø', '\\u00D8')
-#     da_dict_add[key] = da_dict_add[key].replace('Å', '\\u00C5')
-
-
-def update_properties_files_with_new_danish_values(danish_dict_additions, path_prefix):
-    for uid in danish_dict_additions.keys():
+def update_properties_files_da(da_dict_add, path_prefix):
+    for uid in da_dict_add.keys():
+        print 'uid:' + uid
         path, label = uid.split('}')
         path = path[1:]
         full_path = join(path_prefix, path)
         full_path = full_path.replace(EXTENSION, '_da' + EXTENSION)
-        # print full_path
-        
         print uid
         if isfile(full_path):
             print 'File already exists'
             with open(full_path, 'a') as f:
-                f.write('\n' + label + '=' + danish_dict_additions[uid] + '\n')
+                f.write('\n' + label + '=' + da_dict_add[uid] + '\n')
         else:
             print 'Creating new file'
             if not exists(dirname(full_path)):
                 makedirs(dirname(full_path))
             with open(full_path, 'w') as f:
-                f.write(label + '=' + danish_dict_additions[uid] + '\n')
-            # raw_input('tryk')
+                f.write(label + '=' + da_dict_add[uid] + '\n')
 
-# update_properties_files_with_new_danish_values(da_dict_add, translations_source)
+def extract(prefix, filenames, da_dictionary):
+    sorted_filenames = sorted(filenames, key = lambda filename: filename[0])
+    generate_csv(prefix, sorted_filenames, da_dictionary)
+
+def inject(prefix, translation, en_dictionary, da_dictionary):
+    csv = read_properties_from_csv(prefix + '_properties.csv', CSV_SEPARATOR)
+    csv_dictionary = dict((x[3], x[2]) for x in csv)
+    da_dict_add, mistakes = generate_new_danish_dictionary(en_dictionary, da_dictionary, csv_dictionary)
+    if(args.translate):
+        for key in da_dict_add.keys():
+            da_dict_add[key] = da_dict_add[key].replace('æ', '\\u00E6')
+            da_dict_add[key] = da_dict_add[key].replace('ø', '\\u00F8')
+            da_dict_add[key] = da_dict_add[key].replace('å', '\\u00E5')
+            da_dict_add[key] = da_dict_add[key].replace('Æ', '\\u00C6')
+            da_dict_add[key] = da_dict_add[key].replace('Ø', '\\u00D8')
+            da_dict_add[key] = da_dict_add[key].replace('Å', '\\u00C5')
+    update_properties_files_da(da_dict_add, translation)
+
+def init(prefix, source, translation):
+    filenames = find_files(source, LANG_EXT)
+    filenames_da = find_files(translation, 'da')
+    en_dictionary = dict((x[5], x[1]) for x in filenames)
+    da_dictionary = dict((x[5], x[1]) for x in filenames_da)
+    if(args.extract):
+        extract(prefix, filenames, da_dictionary)
+    if(args.inject):
+        inject(prefix, translation, en_dictionary, da_dictionary)
+
+init('share', share_source, share_translations)
+init('aikau', aikau_source, aikau_translations)
 
